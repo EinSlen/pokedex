5dcc70b8785db8a3417897177903a7296eb7845c
Valentin Damlencourt <valentin_damlencourt@ens.univ-artois.fr>
2024-01-11 09:42:13 +0100

    save

--- app.py
+++ app.py
@@ -78,5 +78,5 @@ def index():
         reponse = show(pokedex.name.data)
         if(reponse == 404):
             return render_template('index.html', form=pokedex, reponse=reponse)
-        return render_template('index.html', form=pokedex, reponse=reponse)
+        return render_template('render.html', form=pokedex, reponse=reponse)
     return render_template('index.html', form=pokedex)
